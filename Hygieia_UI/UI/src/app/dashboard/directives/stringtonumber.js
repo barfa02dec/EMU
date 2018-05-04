ee8eafdb9b18f8be3dfb29ac8978dfb8979fea65
Akhilesh <akhilesh.varma@happiestminds.com>
2018-05-04 12:25:33 +0530

    Added directive convert number to string.

---
+++ Hygieia_UI/UI/src/app/dashboard/directives/stringtonumber.js
@@ -0,0 +1,18 @@
+(function() {
+    'use strict';
+    angular
+        .module(HygieiaConfig.module + '.core')
+        .directive('stringToNumber', function() {
+            return {
+                require: 'ngModel',
+                link: function(scope, element, attrs, ngModel) {
+                    ngModel.$parsers.push(function(value) {
+                        return '' + value;
+                    });
+                    ngModel.$formatters.push(function(value) {
+                        return parseFloat(value, 10);
+                    });
+                }
+            };
+        });
+})();
