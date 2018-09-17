e584afadb50c529ab50ff63620281d699cf2bb41
Akhilesh <akhilesh.varma@happiestminds.com>
2018-09-17 11:53:22 +0530

    Heat map issue fixes.

--- Hygieia_UI/UI/src/app/dashboard/directives/stringtonumber.js
+++ Hygieia_UI/UI/src/app/dashboard/directives/stringtonumber.js
@@ -7,7 +7,14 @@
                 require: 'ngModel',
                 link: function(scope, element, attrs, ngModel) {
                     ngModel.$parsers.push(function(value) {
-                        return '' + value;
+                        if(value){
+                            return '' + value;
+                        }
+                        else{
+                            return value;
+                        }
+
+                        
                     });
                     ngModel.$formatters.push(function(value) {
                         return parseFloat(value, 10);
